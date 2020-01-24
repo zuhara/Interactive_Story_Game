f93043feb45c3879dc3c62d801f3b3152266c79c
zuhara <zuhara@hamon.in>
2020-01-24 11:39:27 +0530

    Corrected spelling mistakes in the map data

--- test_game.py
+++ test_game.py
@@ -3,7 +3,7 @@ import game
 def test_read_data_correct_map():
     path = "./test_data/test_game.map"
     a = game.read_data(path)
-    e = {'map': {1: {'north': 2, 'south': 3, 'east': 0, 'west': 0, 'room': 'Hall', 'objects': ['key', 'watch']}, 2: {'north': 0, 'south': 1, 'east': 3, 'west': 0, 'room': 'Kitchen', 'objects': ['knife', 'spoon']}, 3: {'north': 1, 'south': 0, 'east': 0, 'west': 2, 'room': 'Garden', 'objects': ['shovel', 'jug']}}, 'players': {'player1': [1, ['knife', 'shovel']], 'player2': [3, ['spoon', 'key', 'watch', 'knife', 'shovel', 'jug']]}, 'instructions': '\n\n---------Instructions---------\n\nYou can go the directions  north  south  east  west \nif you wana navigate type the direction with go..... Eg  go north\nif you wana take somthing from the room type the object name with the word -- take....  Eg  take key \n\nif you wana exit the game type ---- exit\n'}
+    e = {'map': {1: {'north': 2, 'south': 3, 'east': 0, 'west': 0, 'room': 'Hall', 'objects': ['key', 'watch']}, 2: {'north': 0, 'south': 1, 'east': 3, 'west': 0, 'room': 'Kitchen', 'objects': ['knife', 'spoon']}, 3: {'north': 1, 'south': 0, 'east': 0, 'west': 2, 'room': 'Garden', 'objects': ['shovel', 'jug']}}, 'players': {'player1': [1, ['knife', 'shovel']], 'player2': [3, ['spoon', 'key', 'watch', 'knife', 'shovel', 'jug']]}, 'instructions': '\n\n---------Instructions---------\n\nYou can move the directions  north  south  east  west \nif you want to navigate type the direction with the word go..... Eg  go north\nif you want to take somthing from the room type the object name with the word take....  Eg  take key \n\nif you want to exit the game type.... exit\n'}
     assert a == e
 
 def test_read_data_wrong_map():
@@ -65,5 +65,5 @@ def test_play_wrong_command():
 def test_instruction():
     data = game.read_data("./test_data/test_game.map")
     a = game.instruction(data)
-    e = '\n\n---------Instructions---------\n\nYou can go the directions  north  south  east  west \nif you wana navigate type the direction with go..... Eg  go north\nif you wana take somthing from the room type the object name with the word -- take....  Eg  take key \n\nif you wana exit the game type ---- exit\n'
+    e = '\n\n---------Instructions---------\n\nYou can move the directions  north  south  east  west \nif you want to navigate type the direction with the word go..... Eg  go north\nif you want to take somthing from the room type the object name with the word take....  Eg  take key \n\nif you want to exit the game type.... exit\n'
     assert a == e
